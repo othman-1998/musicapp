c30deff1eb0ade5da2e90e4a795ef9ae87e3ed45
outman1998 <outmanmounir98@gmail.com>
2024-06-01 16:44:01 +0200

    changes

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -12,9 +12,7 @@ import { playPause, setActiveSong } from '../redux/features/playerSlice';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 
-const TopChartCard = ({song, index}) => {
-
-  console.log(song)
+const TopChartCard = ({song, index, isPlaying, activeSong, handlePauseClick, handlePlayClick}) => {
 
   return (
     <div className='w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2'>
@@ -32,7 +30,7 @@ const TopChartCard = ({song, index}) => {
 
         <div className='flex-1 flex flex-col justify-center mx-3'> 
 
-          <Link to={`/songs/${song.key}`}>
+          <Link to={`/songs/${song?.id}`}>
             <p className='text-xl font-bold text-white'>
               {song?.attributes?.name} 
             </p>
@@ -47,6 +45,14 @@ const TopChartCard = ({song, index}) => {
         </div>
 
       </div>
+
+      <PlayPause 
+        isPlaying={isPlaying} 
+        activeSong={activeSong} 
+        song={song} 
+        handlePause={handlePauseClick} 
+        handlePlay={handlePlayClick} 
+      />
     </div>
   )
 }
@@ -66,15 +72,11 @@ const TopPlay = () => {
 
   const topPlays = data?.data?.slice(0,5);
 
-  console.log(topPlays)
-
   const handlePauseClick = () => {
-
     dispatch(playPause(false));
-    
   };
 
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, index) => {
     dispatch(setActiveSong({ song, data, index }));
     dispatch(playPause(true));
   };
@@ -94,7 +96,15 @@ const TopPlay = () => {
 
         <div className='mt-4 flex flex-col gap-1'>
           {topPlays?.map((song, index) => (
-            <TopChartCard key={index} song={song} index={index} />
+            <TopChartCard 
+              key={index} 
+              song={song} 
+              index={index} 
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, index)}
+            />
           ))}
         </div>
 
