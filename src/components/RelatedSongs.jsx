788b6704b633111ab0c5d758448c8f368219feba
outman1998 <outmanmounir98@gmail.com>
2024-06-09 15:27:07 +0200

    changes

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,38 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
-);
+import React from 'react'
+import SongBar from './SongBar';
 
-export default RelatedSongs;
+
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+  artistId
+}) => {
+
+  console.log(data?.tracks);
+
+  return (
+    <div className='text-white flex flex-col'>
+      <h2 className='font-bold text-3xl text-white'>Related songs</h2>
+
+      <div className='mt-6 w-full flex flex-col'>
+        {data?.tracks?.map((song, index) => (
+          <SongBar 
+          key={`${song.key}-${artistId}`}
+          song={song}
+          index={index}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          handlePauseClick={handlePauseClick}
+          handlePlayClick={handlePlayClick}
+          />
+        ))}
+      </div>
+
+    </div>
+  )
+}
+
+export default RelatedSongs
--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -3,9 +3,15 @@ import { Link } from 'react-router-dom';
 
 import PlayPause from './PlayPause';
 
-const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
-  <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
-    <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
+const SongBar = ({ song, index, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => {
+
+
+  console.log(song)
+
+
+  return (
+  <div className={`w-full flex flex-row items-center hover:bg-[#2e2a31] ${activeSong?.title === song?.title ? 'bg-[#2e2a31]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
+    <h3 className="font-bold text-base text-white mr-3">{index + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
       <img
         className="w-20 h-20 rounded-lg"
@@ -14,11 +20,11 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, h
       />
       <div className="flex-1 flex flex-col justify-center mx-3">
         {!artistId ? (
-          <Link to={`/songs/${song.key}`}>
+          // <Link to={`/songs/${song.key}`}>
             <p className="text-xl font-bold text-white">
               {song?.title}
             </p>
-          </Link>
+          // </Link>
         ) : (
           <p className="text-xl font-bold text-white">
             {song?.attributes?.name}
@@ -36,11 +42,11 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, h
           activeSong={activeSong}
           song={song}
           handlePause={handlePauseClick}
-          handlePlay={() => handlePlayClick(song, i)}
+          handlePlay={() => handlePlayClick(song, index)}
         />
       )
       : null}
   </div>
-);
+)};
 
 export default SongBar;
