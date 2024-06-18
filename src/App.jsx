fd6b3f8b661810ba090a97b5b743e3496a5be910
outman1998 <outmanmounir98@gmail.com>
2024-06-18 12:49:31 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   const { activeSong } = useSelector((state) => state.player);
 
   return (
-    <div className="relative flex">
+    <div className="flex">
       <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#000000]">
         <Searchbar />
